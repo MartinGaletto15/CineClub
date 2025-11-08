4717dc883793699f3151b9baf2cd336ddc6ecf02
KevinKener <kevinkener.kick@outlook.com>
2025-11-08 15:14:54 -0300

    Swagger + Azure

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -115,11 +115,8 @@ builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseMiddleware<GlobalExceptionHandlingMiddleware>();
 
